1a78260ad7415fbf375582184e9997fb400e985c
Enrico Valbuena <enricovalbuena@gmail.com>
2017-03-27 14:14:49 -0700

    Added React Router and Axios features

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,8 @@ import {Router, Route, browserHistory} from 'react-router';
 import './App.css';
 import Head from './Head';
 import Body from './Body';
+import Display from './Display';
+import Api_Demo from './Api_Demo';
 
 class App extends Component {
   render() {
@@ -10,6 +12,8 @@ class App extends Component {
       <Router history={browserHistory}>
         <Route component={Head}>
           <Route path="/" component={Body} />
+          <Route path="/display" component={Display} />
+          <Route path="/apidemo" component={Api_Demo} />
         </Route>
       </Router>
     );
--- client/src/components/Head.js
+++ client/src/components/Head.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Head.css';
+import {Link} from 'react-router';
 
 class Head extends Component {
   render() {
@@ -9,6 +10,11 @@ class Head extends Component {
         <div className="Head-header">
           <img src={logo} className="Head-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <div>
+            <Link to="/" className="link">Home</Link>
+            <Link to="/display" className="link">Alternate Component</Link>
+            <Link to="/apidemo" className="link">Api Example</Link>
+          </div>
         </div>
         <div className="Site-page">
           {this.props.children}
